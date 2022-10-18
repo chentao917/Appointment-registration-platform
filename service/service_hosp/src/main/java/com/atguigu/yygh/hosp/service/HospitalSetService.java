49a001f37d27124c1438decfcc306e8a367ea233
chentao917 <chentao917@foxmail.com>
2022-10-18 21:42:21 +0800

    Last Commit

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/HospitalSetService.java
@@ -0,0 +1,14 @@
+package com.atguigu.yygh.hosp.service;
+
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.order.SignInfoVo;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.stereotype.Service;
+
+
+public interface HospitalSetService extends IService<HospitalSet> {
+    String getSignKey(String hoscode);
+
+    //获取医院签名信息
+    SignInfoVo getSignInfoVo(String hoscode);
+}
